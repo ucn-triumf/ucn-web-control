37955c8e2a3ccd093f284deec3227f08592be916
Thomas Lindner <lindner@triumf.ca>
2017-08-25 11:12:45 -0700

    Add more interesting columns
    Add a parallel txt file output

--- create_runlog.py
+++ create_runlog.py
@@ -15,8 +15,8 @@ class runlog:
 
 
 
-  def initialize_html(self, outfile):
-    outfile.write(
+  def initialize_files(self, htmlfile,txtfile):
+    htmlfile.write(
         "<!DOCTYPE html>\n "
         " <html>\n "
         "\n"
@@ -49,16 +49,33 @@ class runlog:
         "<body>\n"        
         )
 
-    outfile.write('<table id="nicetable">\n')
-    outfile.write('<tr> <td> Run Number </td>  <td> Start Time </td> <td> End Time </td>">\n')
-    outfile.write('<td> He-3 events </td>  <td> Li-6 events </td> <td> Comments </td> </tr>">\n')
+    htmlfile.write('<table id="nicetable">\n')
+    htmlfile.write('<tr> <td> Run Number </td>  <td> Start </td> <td> End Time </td>\n')
+    htmlfile.write('<td> <pre> Beamline \n enabled?</pre> </td> \n')
+    htmlfile.write('<td> <pre> Beam on \n time (s)</pre> </td> \n')
+    htmlfile.write('<td> <pre> UCN Valve \n open (s)</pre> </td> \n')
+    htmlfile.write('<td> He-3 events </td>  <td> Li-6 events </td> <td> Comments </td> </tr>\n')
+    
+    txtfile.write("Run number, Start/End Time, Beamline enabled?, Beam on time, UCN Valve Open, ")
+    txtfile.write("He-3 events, Li-6 events, Comments")
     return
 
-  def odb2html(self, odb, outfile):
+  def writecolumn(self,htmlfile,txtfile,value):
+
+    htmlfile.write("<td>" + value + "</td>");
+    txtfile.write(value + ", ");
+
+  def odb2html(self, odb, htmlfile,txtfile):
     # Run number / data logged flag / Start time / EBuilder events / Fragment list / comments
     run_number = odb['Runinfo']['Run number']
     start_time = odb['Runinfo']['Start time']
     stop_time = odb['Runinfo']['Stop time']
+    beamline_enabled = "no"
+    if odb['Equipment']['BeamlineEpics']['Variables']['Measured'][1] and odb['Equipment']['BeamlineEpics']['Variables']['Measured'][3]:
+        beamline_enabled = "yes"
+
+    beamon_time = odb['Equipment']['BeamlineEpics']['Variables']['Measured'][30] *0.000888111
+    valveopen_time = odb['Equipment']['UCNSequencer']['Settings']['valveOpenTime']
     he3_events = odb['Equipment']['FEV785']['Statistics']['Events sent']
     li6_events = odb['Equipment']['FEV1720I']['Statistics']['Events sent']
 
@@ -66,16 +83,24 @@ class runlog:
     if "Edit on start" in odb['Experiment']:
       comment = odb['Experiment']['Edit on start']['Comment']
 
-    txt = "<tr>"
-    txt += "<td>" + str(run_number).rjust(7) + "</td>"
-    txt += "<td>" + str(start_time).ljust(27) + "</td>"
-    txt += "<td>" + str(stop_time).ljust(27) + "</td>"
-    txt += "<td>" + str(he3_events) + "</td>"
-    txt += "<td>" + str(li6_events) + "</td>"
-    txt += "<td>" + comment + "</td>"
-    txt += "</tr>"
-    outfile.write(txt + "\n");
-    return txt
+    htmlfile.write("<tr>")
+    self.writecolumn(htmlfile,txtfile,str(run_number))
+    self.writecolumn(htmlfile,txtfile,str(start_time))
+    self.writecolumn(htmlfile,txtfile,str(stop_time))
+    self.writecolumn(htmlfile,txtfile,beamline_enabled)
+    self.writecolumn(htmlfile,txtfile,str(round(beamon_time,2)))
+    self.writecolumn(htmlfile,txtfile,str(valveopen_time))
+    self.writecolumn(htmlfile,txtfile,str(he3_events))
+    self.writecolumn(htmlfile,txtfile,str(li6_events))
+    self.writecolumn(htmlfile,txtfile,comment)
+    htmlfile.write("</tr>\n")
+
+    txtfile.write("\n")
+
+
+    
+
+    return
 
   def odbFromJson(self, filename):
     with open(filename, "r") as file:
@@ -86,8 +111,9 @@ class runlog:
 
   def bulkupload(self):
 
-    outfile = open('/home/ucn/online/ucn-web-control/runlist.html','w')
-    self.initialize_html(outfile)
+    htmlfile = open('/home/ucn/online/ucn-web-control/runlist.html','w')
+    txtfile = open('/home/ucn/online/ucn-web-control/runlist.txt','w')
+    self.initialize_files(htmlfile,txtfile)
 
 
     seen_datadirs = []
@@ -104,10 +130,10 @@ class runlog:
       print filename
       
 
-      self.odb2html(odb,outfile)
+      self.odb2html(odb,htmlfile,txtfile)
 
-    outfile.write('</table>\n')
-    outfile.write("</body>\n"
+    htmlfile.write('</table>\n')
+    htmlfile.write("</body>\n"
                   "</html>\n")
 
 
